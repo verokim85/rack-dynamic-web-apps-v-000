ff835827e548b04366d3770c31cd00772da90548
Veronica Kim <vero.a.kim@gmail.com>
2018-05-12 15:02:10 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,12 @@ class Application
     num_1 = Kernel.rand(1..20)
     num_2 = Kernel.rand(1..20)
     num_3 = Kernel.rand(1..20)
-    resp.write "Hello, World"
+
+    if num_1==num_2 && num_2==num_3
+      resp.write "You Win"
+    else
+      resp.write "You Lose"
+
     resp.finish
   end
 
