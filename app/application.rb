b355163852bf2dc7b1ce7a91624cac2bb6eb2995
Veronica Kim <vero.a.kim@gmail.com>
2018-05-12 15:05:07 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,9 +3,9 @@ class Application
   def call(env)
     resp = Rack::Response.new
 
-    num_1 = Kernel.rand(1..20)
-    num_2 = Kernel.rand(1..20)
-    num_3 = Kernel.rand(1..20)
+    num_1 = Kernel.rand(1..2)
+    num_2 = Kernel.rand(1..2)
+    num_3 = Kernel.rand(1..2)
 
       resp.write "#{num_1}"
       resp.write "#{num_2}"
@@ -16,7 +16,7 @@ class Application
     else
       resp.write "You Lose"
     end
-    
+
     resp.finish
   end
 
